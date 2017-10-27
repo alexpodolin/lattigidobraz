9b7fb9bb156aaaf027753a9fa0f3e7d7e84a60e4
alexpodolin <alex.podolin@gmail.com>
2017-10-27 09:41:03 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+var myIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slider__slide");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+
+    myIndex++;
+    if (myIndex > x.length) 
+      {myIndex = 1}  
+
+      if (x[myIndex-1]) {
+        x[myIndex-1].style.display = "block";
+      }  
+      
+      setTimeout(carousel, 5000);    
+}
+
+carousel();
