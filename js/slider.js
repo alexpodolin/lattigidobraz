2aa0794092ac6a929a7479e99fdefde153cf81af
Aleksandr Podolin <alex.podolin@gmail.com>
2017-11-27 20:18:18 +0300

    27.11.17

--- js/slider.js
+++ js/slider.js
@@ -15,7 +15,7 @@ function carousel() {
         x[myIndex-1].style.display = "block";
       }  
       
-      setTimeout(carousel, 8000);    
+      setTimeout(carousel, 7000);    
 }
 
 carousel();
